d885b11269f0dc912eab149f24dd239a7e4c8e94
AmarnathaGowda <54569432+AmarnathaGowda@users.noreply.github.com>
2022-05-15 05:14:19 +0530

    The recommendation filters are loosely coupled.

---
+++ src/main/java/io/datajek/spring/basics/movieRecommendationsSystem/Lession2/MovieRecommendationsSystemApplication.java
@@ -0,0 +1,22 @@
+package io.datajek.spring.basics.movieRecommendationsSystem.Lession2;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class MovieRecommendationsSystemApplication {
+
+	public static void main(String[] args) {
+
+		//passing name of the filter as constructor argument
+		RecommenderImplementation recommender = new RecommenderImplementation(new ContentBasedFilter());
+
+		//call method to get recommendations
+		String[] result = recommender.recommendMovies("Finding Dory");
+
+		//display results
+		System.out.println(Arrays.toString(result));
+	}
+
+}
