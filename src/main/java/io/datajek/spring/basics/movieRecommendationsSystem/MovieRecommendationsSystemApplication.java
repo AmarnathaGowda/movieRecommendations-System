2ff6e598a1b00de2307a2077b642870e486272f5
AmarnathaGowda <54569432+AmarnathaGowda@users.noreply.github.com>
2022-05-15 04:07:10 +0530

    Initial environment setup

---
+++ src/main/java/io/datajek/spring/basics/movieRecommendationsSystem/MovieRecommendationsSystemApplication.java
@@ -0,0 +1,13 @@
+package io.datajek.spring.basics.movieRecommendationsSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieRecommendationsSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieRecommendationsSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/datajek/spring/basics/movieRecommendationsSystem/MovieRecommendationsSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package io.datajek.spring.basics.movieRecommendationsSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieRecommendationsSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
